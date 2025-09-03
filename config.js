38f1191e2ae7fef798727f41cbff97015808f00e
DominicDev <s0l0estudi4r@gmail.com>
2025-09-03 17:12:53 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'ED SHEERAN';
+export let nameSongs = [
+	"SHAPE OF YOU", /*1*/
+	"BAD HABITS", /*2*/
+	"THINKING OUT LOUD", /*3*/
+	"PERFECT", /*4*/
+	"PHOTOGRAPH", /*5*/
+	"GIVE ME LOVE", /*6*/
+	"KISS ME", /*7*/
+	"ALL OF THE STARS", /*8*/
+	"THE A TEAM", /*9*/
+	"CASTLE ON THE HILL", /*10*/
+	"WHAT DO I KNOW? ", /*11*/
+	"DIVE", /*12*/
+	"I SEE FIRE", /*13*/
+	"GALWAY GIRL", /*14*/
+	"NANCY MULLIGAN ", /*15*/
+	"BEAUTIFUL PEOPLE", /*16*/
+	"I DON'T CARE", /*17*/
+	"LEGO HOUSE", /*18*/
+	"SING", /*19*/
+	"BLOODSTREAM", /*20*/
+]
